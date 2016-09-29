c1a9b9b954722da8b9464ad60f20197e6376de61
1-718-8 <1-718-8@19-717-8-PC>
2016-09-29 18:06:30 +0900

    첫번 째 commit

---
+++ projectBy3/src/main/java/config/ApplicationConfig.java
@@ -0,0 +1,62 @@
+package config;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@PropertySource("classpath:/config/dbconfig.properties")
+@Configuration
+public class ApplicationConfig {
+	static Logger logger = LoggerFactory.getLogger(ApplicationConfig.class);
+	
+	@Bean
+	public PlatformTransactionManager transactionManager(DataSource ds){
+		PlatformTransactionManager tm = new DataSourceTransactionManager(ds);
+		return tm;		
+	}
+	
+	@Bean
+	public DataSource dataSource(@Value("${db.driverClassName}") String driver, 
+													 @Value("${db.url}") String url,
+													 @Value("${db.username}") String id, 
+													 @Value("${db.password}") String pass) {
+
+		logger.trace("driver:{}", driver);
+
+		BasicDataSource ds = new BasicDataSource();
+
+		ds.setDriverClassName(driver);
+		ds.setUrl(url);
+		ds.setUsername(id);
+		ds.setPassword(pass);
+
+		return ds;
+	}
+
+	@Bean
+	public SqlSessionFactoryBean sqlSessionFactoryBean(DataSource ds) {
+		SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+		bean.setDataSource(ds);
+		String loc = "mybatis/mybatis-config.xml";
+		bean.setConfigLocation(new ClassPathResource(loc));
+		return bean;
+	}
+
+	@Bean
+	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactoryBean sfb) throws Exception {
+		SqlSessionTemplate template = new SqlSessionTemplate(sfb.getObject());
+		return template;
+
+	}
+}
