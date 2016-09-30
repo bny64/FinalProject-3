d3e9710b271bfe831884ef3709f1e5780c356d60
EG-717-8 <EG-717-8@EG-717-8-PC>
2016-09-30 14:31:33 +0900

    hi

--- projectBy3/src/main/java/githubtest/gitHubTest.java
+++ projectBy3/src/main/java/githubtest/gitHubTest.java
@@ -3,7 +3,7 @@ package githubtest;
 public class gitHubTest {
 	public static void main(String[] args) {
 		System.out.println("hello");
-		System.out.println("hi");
+		System.out.println("hi2");
 		System.out.println("hello2");
 
 	}
