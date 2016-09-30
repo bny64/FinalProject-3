ff2b2e46cbcd9ec0e9e65a67cada0f98c39411e4
1-718-8 <1-718-8@19-717-8-PC>
2016-09-30 14:41:16 +0900

    user dto

--- projectBy3/src/main/java/dto/Users.java
+++ projectBy3/src/main/java/dto/Users.java
@@ -1,9 +1,19 @@
 package dto;
 
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class Users {
-
-		
+	Integer userNo;
+	String userId;
+	String password;
+	String nickname;
+	String userName;
+	String tel;
+	String email;
+	@DateTimeFormat(pattern="yy-MM-dd")
+	Date birthdate;
 }
