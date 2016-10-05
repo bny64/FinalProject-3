6316eebc03630a8dbd9308db9ec3d7d1fad63fbb
1-718-8 <1-718-8@19-717-8-PC>
2016-10-05 11:44:39 +0900

    남열 : admin, board dto 생성 main-controller 생성

--- projectBy3/src/main/java/controller/MainController.java
+++ projectBy3/src/main/java/controller/MainController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
@@ -17,5 +19,5 @@ public class MainController {
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 		binder.registerCustomEditor(Date.class, new CustomDateEditor(format, true));
 	}
-	
+		
 }
