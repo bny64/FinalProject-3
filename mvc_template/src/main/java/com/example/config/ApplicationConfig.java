9650a02c0ecb7d49b1b164419758b0b7b0927019
1-718-8 <1-718-8@19-717-8-PC>
2016-10-10 12:07:34 +0900

    mvctemplate

---
+++ mvc_template/src/main/java/com/example/config/ApplicationConfig.java
@@ -0,0 +1,12 @@
+package com.example.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+//Spring Core에 대한 설정
+@Configuration
+public class ApplicationConfig {
+
+}
