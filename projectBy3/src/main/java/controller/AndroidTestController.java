144fad62fb36ab18a5a3da4edf7a2ac87d73e8dc
1-718-8 <1-718-8@19-717-8-PC>
2016-10-24 12:25:27 +0900

    11

--- projectBy3/src/main/java/controller/AndroidTestController.java
+++ projectBy3/src/main/java/controller/AndroidTestController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -10,8 +12,14 @@ public class AndroidTestController {
 
 	static Logger logger = LoggerFactory.getLogger(AndroidTestController.class);
 	
-	@RequestMapping("android")
+	@RequestMapping("/android")
 	public void androidTest(){
 		logger.trace("안드로이드 접근 성공");
+	}
+	
+	@RequestMapping("/android2")
+	public void androidTest2(HttpServletRequest request){
+		logger.trace("위도 : {}", request.getParameter("latitude"));
+		logger.trace("경도 : {}", request.getParameter("longitude"));
 	}
 }
