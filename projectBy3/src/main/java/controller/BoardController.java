975e534de3aa25248f8f3be78208a53a354b0306
EG-717-8 <EG-717-8@EG-717-8-PC>
2016-10-27 15:34:26 +0900

    은구 - 커밋

--- projectBy3/src/main/java/controller/BoardController.java
+++ projectBy3/src/main/java/controller/BoardController.java
@@ -127,6 +127,7 @@ public class BoardController {
 
 	private static final String uploadDir = "C:/Users/EG-717-8/git/FinalProject-3/projectBy3/src/main/webapp/WEB-INF/assets/images/userImages";
 	//private static final String uploadDir = "E:/sts-bundle/pivotal-tc-server-developer-3.1.5.RELEASE/server/wtpwebapps/projectBy3/WEB-INF/assets/images";
+	
 	@RequestMapping(value="/writeBoard", method=RequestMethod.POST)
 	public String writeBoardPost(HttpSession session, Board board, @RequestParam MultipartFile file) throws IllegalStateException, IOException{
 		logger.trace("class : BoardController, method : writeBoardPost");	
