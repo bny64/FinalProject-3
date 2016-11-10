f18f8609569632d2066b1b8860164482fba5d88c
1-718-8 <1-718-8@28-717-8-PC>
2016-11-10 15:31:51 +0900

    민국 - detailBoard수정

--- projectBy3/src/main/java/controller/BoardController.java
+++ projectBy3/src/main/java/controller/BoardController.java
@@ -89,9 +89,8 @@ public class BoardController {
 	}
 
 	// 민국, 위치
-	// private static final String uploadDir =
-	// "C:/Users/1-718-8/git/FinalProject-3/projectBy3/src/main/webapp/WEB-INF/assets/images/userImages/";
-	private static final String uploadDir = "C:/Users/EG-717-8/git/FinalProject-3/projectBy3/src/main/webapp/WEB-INF/assets/images/userImages/";
+	 private static final String uploadDir ="C:/Users/1-718-8/git/FinalProject-3/projectBy3/src/main/webapp/WEB-INF/assets/images/userImages/";
+	//private static final String uploadDir = "C:/Users/EG-717-8/git/FinalProject-3/projectBy3/src/main/webapp/WEB-INF/assets/images/userImages/";
 	// private static final String uploadDir =
 	// "E:/sts-bundle/pivotal-tc-server-developer-3.1.5.RELEASE/server/wtpwebapps/projectBy3/WEB-INF/assets/images";
 
